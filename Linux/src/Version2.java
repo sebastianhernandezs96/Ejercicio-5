8bb7e6c7aad0bd70b54c26f32037701bf6d92d5d
sebastianhernandezs96 <jshernandez82@misena.edu.co>
2016-02-29 22:58:06 -0500

    Version 2

---
+++ Linux/src/Version2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Version2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("suse");
+        System.out.println("mandriva");
+        System.out.println("versiones numero 2");
+    }
+    
+}
